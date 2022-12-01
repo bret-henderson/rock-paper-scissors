1f4407ff56c06303a7e8d43f196ec0857ee758b7
bret-henderson <98489557+bret-henderson@users.noreply.github.com>
2022-11-30 19:49:13 -0500

    Create getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    let ranNum = Math.floor(Math.random()*3)
+    if (ranNum == 0)
+        return 'Rock'
+    else if (ranNum == 1)
+        return 'Paper'
+    else
+        return 'Scissors'
+}
